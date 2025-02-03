a360fad9cb169c6981150987e36692c379336e17
dudewithoneleg <galvancromeo@gmail.com>
2025-02-03 01:24:24 -0600

    added marketplace home goods search and route

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   get "/facebook_free_search", to: "facebook#marketplace_free_search"
   get "/facebook_garden_search", to: "facebook#marketplace_garden_search"
   get "/facebook_hobbies_search", to: "facebook#marketplace_hobbies_search"
+  get "/facebook_home_search", to: "facebook#marketplace_home_search"
   get "/facebook_profile", to: "facebook#facebook_profile"
   get "/facebook_friends", to: "facebook#facebook_friends"
   get "/facebook_location_id_search", to: "facebook#facebook_location_id_search"
