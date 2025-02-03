9de77e1f47b51ad78e8c17e73bfaec94824a5fc1
dudewithoneleg <galvancromeo@gmail.com>
2025-02-03 01:18:39 -0600

    added marketplace hooby search and route

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
   get "/facebook_family_search", to: "facebook#marketplace_family_search"
   get "/facebook_free_search", to: "facebook#marketplace_free_search"
   get "/facebook_garden_search", to: "facebook#marketplace_garden_search"
+  get "/facebook_hobbies_search", to: "facebook#marketplace_hobbies_search"
   get "/facebook_profile", to: "facebook#facebook_profile"
   get "/facebook_friends", to: "facebook#facebook_friends"
   get "/facebook_location_id_search", to: "facebook#facebook_location_id_search"
