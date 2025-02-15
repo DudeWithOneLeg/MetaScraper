7e4da0ab7e3e7452e0d91ba92be133203a6dd27c
dudewithoneleg <galvancromeo@gmail.com>
2025-02-15 02:33:45 -0600

    structured pages response results

--- app/controllers/facebook/pages/pages_controller.rb
+++ app/controllers/facebook/pages/pages_controller.rb
@@ -102,9 +102,28 @@ module Facebook
         doc_id = "9506048516125027"
 
         response = HTTParty.post("https://www.facebook.com/api/graphql?variables=#{variables}&doc_id=#{doc_id}")
-        puts response.body
+        body = JSON.parse(response.body)
+        response_results = body.dig("data", "serpResponse", "results", "edges")
+        results = []
 
-        render json: JSON.parse(response.body)
+        response_results.each do |result|
+            info = result.dig("rendering_strategy", "view_model", "profile")
+            id = info.dig("id")
+            name = info.dig("name")
+            url = info.dig("url")
+            profile_url = info.dig("profile_url")
+            profile_picture = info.dig("profile_picture")
+
+            results.push({
+                id: id,
+                name: name,
+                url: url,
+                profile_url: profile_url,
+                profile_picture: profile_picture
+            })
+        end
+
+        render json: results
       end
     end
   end
